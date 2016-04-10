76021c2f09bb82c5bb5a0221d8afe99247ad8d42
lasewun <lasewun@gmail.com>
2016-04-10 16:47:11 +1200

    Before checking of code

--- Assignments/Assignment 1a/Assignment 1a P3 redone.py
+++ Assignments/Assignment 1a/Assignment 1a P3 redone.py
@@ -2,37 +2,37 @@
 
 #Variables
 def intro():                                                                                       #Intro
-    print('     Weclome to Mad libs!')
+    print('     Welcome to Mad libs!')
     print('     Please provide the following to help')
     print('     create a new story')
 
-def userInputValues():                                                                             #funcation for dictionarty of userinputs
+def userInputValues():                                                                             #funcation for dictionary of userinputs
     inputValues = dict(username=0, plural_noun=0, integer_value=0, body_part=0, verb=0)            #list of values to have values updated by user
-    inputValues.update(username     =(str(input('Please enter name: '))))                          #update of value with user input
-    inputValues.update(plural_noun  =(str(input('Please enter plural noun: '))))
-    inputValues.update(integer_value=(str(input('Please enter integer value: '))))
-    inputValues.update(body_part    =(str(input('Please enter body part: '))))
-    inputValues.update(verb         =(str(input('Please enter verb: '))))
+    inputValues.update(username     =(str(input('Please enter a name: '))))                        #update of value with user input
+    inputValues.update(plural_noun  =(str(input('Please enter a plural noun: '))))
+    inputValues.update(integer_value=(str(input('Please enter an integer value: '))))
+    inputValues.update(body_part    =(str(input('Please enter a body part: '))))
+    inputValues.update(verb         =(str(input('Please enter a verb: '))))
     return inputValues
 
-def findValueField(value):                                                                         #function that scans 'story' for values to be replaced
-    entryList   = list()                                                                           #start of list-empty
-    start       = value.find('{')                                                                  #scans for '{' which is pointer to value to be replaced
-    end         = value.find('}')                                                                  #scans for '}' which is end of pointer to value to be replaced
-    entires     = value[start: end]                                                                #specifies that value in pointers gets replaced
-    entryList.append(entires)                                                                      #appends value
-    return set(entryList)                                                                          #returns list - updated
+# def findValueField(value):                                                                         #function that scans 'story' for values to be replaced
+#     entryList   = list()                                                                           #creates list - empty
+#     start       = value.find('{')                                                                  #scans for '{' which is pointer to value to be replaced
+#     end         = value.find('}')                                                                  #scans for '}' which is end of pointer to value to be replaced
+#     entry       = value[start: end]                                                                #specifies that value in pointers gets replaced
+#     entryList.append(entry)                                                                        #appends value
+#     return set(entryList)                                                                          #returns list - updated
 
-def printStory(printedStory):                                                                      #function that prints story with userinput values
-    findValueField(printedStory)                                                                   #calls findValueField function
-    inputValues = userInputValues()                                                                #calls userInputValues
-    story       = printedStory.format(**inputValues)                                               #injects/replaces values from user input
+def printStory(story, inputValues):                                                                #function that prints story with userinput values
+    #findValueField(story)                                                                          #calls findValueField function
+    story       = story.format(**inputValues)                                                      #injects/replaces values from user input
     print(story)                                                                                   #prints story
 
 def main():                                                                                        #main function
+    inputValues = userInputValues()                                                                #calls userInputValues
     story = '''
         ----------------------------------------
-        Here is your story
+        Here is your story lolol
         The famous explorer {username} had nearly given up a life long quest to find
         the lost city of {plural_noun} when one day, the {plural_noun} found the explorer.
         Surrounded by {integer_value} {plural_noun} a tear came to {username}'s {body_part}.
@@ -42,7 +42,7 @@ def main():                                                                     
         ----------------The end-----------------
             '''
     intro()                                                                                        #call of intro function
-    printStory(story)                                                                              #calls printStory function using story
+    printStory(story, inputValues)                                                                 #calls printStory function using story
     return
 
 #code
